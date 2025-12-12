9b53afdbc7f8fbdb07ca22e527e923f31e737fe8
Ruzal <ruzals661@gmail.com>
2025-12-12 19:22:57 +0300

    Add mapping class

--- src/db/abstracts/db.py
+++ src/db/abstracts/db.py
@@ -11,17 +11,27 @@ class AbstractStorage(ABC, Generic[T]):
         self.client = client
 
     @abstractmethod
-    async def get_objs(self, index):
+    async def get_objs(self, index) -> list:
         pass
 
     @abstractmethod
-    async def save_objs(self, objs, index):
+    async def save_objs(self, objs, index) -> None:
         pass
 
     @abstractmethod
-    async def create_index(self, index, mapping):
+    async def create_index(self, index, mapping) -> None:
         pass
 
     @abstractmethod
-    async def create_mapping(self, index):
+    async def create_mapping(self, index: str) -> dict:
+        # Должен вернуть словарь вида:
+        # mapping = {
+        #   "table_name": {
+        #       "column1": {
+        #           "data_type": "integer",
+        #           "is_primary": True
+        #       },
+        #       ....
+        #   }
+        # }
         pass
--- src/service/mapping.py
+++ src/service/mapping.py
@@ -1,4 +1,5 @@
 from ..db.abstracts.db import AbstractStorage
+import json
 
 
 class Mapping:
@@ -6,10 +7,25 @@ class Mapping:
         self.file_path = file_path
         self.storage = storage
 
-    def save_json_mapping():
-        # TODO: Сделать сохранение mapping в файл
-        pass
+    def save_json_mapping(self, index: str):
+        mapping_add = self.storage.create_mapping(index)
+        old_mapping = self.get_json_mapping()
+        old_mapping[index] = mapping_add[index]
 
-    def get_json_mapping():
-        # TODO: Сделать получение mapping-а из json-файла
-        pass
+        with open(self.file_path, 'w', encoding='utf-8') as file:
+            json.dump(old_mapping, file, ensure_ascii=False, indent=2)
+
+    def get_json_mapping(self):
+        try:
+            with open(self.file_path, 'r', encoding='utf-8') as file:
+                mapping = json.load(file)
+
+            return mapping
+        except (FileNotFoundError, json.JSONDecodeError):
+            return {}
+
+
+    def _extract_row(self, column: dict, column_name: str):
+        column['column'] = column_name
+        return column
+
